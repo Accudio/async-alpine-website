59311d276ef2017dc15ec72c1c02de2d00167a1c
Alistair Shepherd <alistair@serieseight.com>
2023-04-26 11:59:34 +0100

    Added Ganni Responsibility Report to showcase, updated Showcase images

--- src/_data/showcase.js
+++ src/_data/showcase.js
@@ -1,3 +1,5 @@
+// set viewport of chrome to 2000x1336, screenshot and crop out scrollbar
+
 const items = [
 	{
 		"title": "The Good Crisp Company",
@@ -16,9 +18,16 @@ const items = [
 	{
 		"title": "Carl Robertshaw",
 		"url": "https://carlrobertshaw.com",
-		"author": "William Bocchinelli",
+		"author": "William Hibberd",
 		"authorUrl": "https://twitter.com/williamhibberd",
 		"image": "./src/assets/img/carlrobertshaw.png"
+	},
+	{
+		"title": "Ganni Responsibility Report 2022",
+		"url": "https://responsibilityreport2022.ganni.com/",
+		"author": "William Hibberd",
+		"authorUrl": "https://twitter.com/williamhibberd",
+		"image": "./src/assets/img/ganni-responsibility-report.png"
 	}
 ]
 
